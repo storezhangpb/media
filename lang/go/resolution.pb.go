fdebe4d7989191634e1b2c4e0a1cbce7522a5eac
storezhang <storezhang@gmail.com>
2022-03-22 10:50:55 +0800

    分辨率增加宽和高

--- lang/go/resolution.pb.go
+++ lang/go/resolution.pb.go
@@ -1,4 +1,16 @@
 package media
 
+const (
+    Resolution_RESOLUTION_UNSPECIFIED Resolution = 0
+    Resolution_P360                   Resolution = 1
+    Resolution_P480                   Resolution = 2
+    Resolution_P720                   Resolution = 3
+    Resolution_P1080                  Resolution = 4
+    Resolution_K2                     Resolution = 5
+    Resolution_K4                     Resolution = 6
+    Resolution_K8                     Resolution = 7
+    Resolution_ORIGINAL               Resolution = 15
+)
+
 // Resolution 占位
 type Resolution uint32
