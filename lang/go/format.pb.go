23df5e2c9afc583df263e8c61a2a6da3e90e61f3
storezhang <storezhang@gmail.com>
2022-03-22 09:49:28 +0800

    增加格式字符串

---
+++ lang/go/format.pb.go
@@ -0,0 +1,10 @@
+package media
+
+const (
+    Format_FORMAT_UNSPECIFIED Format = 0
+    Format_MP3                Format = 1
+    Format_MP4                Format = 2
+)
+
+// Format 占位
+type Format uint32
